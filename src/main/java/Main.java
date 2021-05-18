e5dfb3b49af20c85964a2e8bd2139085e387494d
student <student@172.32.220.28>
2021-05-18 10:30:25 -0400

    working on gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,42 @@
-import java.io.*;
-import java.awt.geom.Path2D;
-import java.util.ArrayList;
+/**
+ *
+ * @author Mr. Maroney
+ * Setup for JFrame
+ * 
+ */
+import javax.swing.JFrame;	 //class for main canvas(window)
+import java.awt.*;
+import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 
 public class Main
 {
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException
-	{
-		SVGParser test = new SVGParser();
-		ArrayList<Path2D.Double> out = test.parseFile(new File("/Users/student/BlankMap-World.svg"));
+	
+	public static void initializeJFrame(JFrame jf){
+		
+		jf.getContentPane().setBackground(Color.green);	//set color of main window
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes the exit icon work correctly
+		jf.setSize(800, 1000);				 //sets the size of the window
+   
+		jf.setLocationRelativeTo(null);					//centers the window
+		jf.setTitle("Rectangles");				  // sets the title bar
+		jf.setVisible(true);							  // makes the window visible
 	}
+	
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException
+	{
+		JFrame window = new JFrame();			//makes a jframe
+		
+		//LettersMove lm = new LettersMove();
+	   // window.add(lm);
+		
+		JPanelClass jp = new JPanelClass();
+		window.add(jp);
+	   
+		initializeJFrame(window);		  //initializes the window to your settings
+
+	  
+	}
+	
 }
